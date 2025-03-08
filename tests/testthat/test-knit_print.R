a42ca009600df10f98404bda646f6c2cf0b47abe
Bill Denney <wdenney@humanpredictions.com>
2025-03-08 12:04:32 -0500

    Remove |> based on CRAN request

--- tests/testthat/test-knit_print.R
+++ tests/testthat/test-knit_print.R
@@ -1,11 +1,7 @@
 test_that("knit_print.tab_tibble (and implicitly knit_print.tab_list and print_tabtibble.tab_pander)", {
-  d_tab <-
-    mtcars |>
-    tidyr::nest(table = !"cyl") |>
-    dplyr::mutate(
-      caption = glue::glue("Cars with {cyl} cylinders")
-    ) |>
-    new_tab_tibble()
+  d_tab_prep <- tidyr::nest(mtcars, table = !"cyl")
+  d_tab_prep <- dplyr::mutate(d_tab_prep, caption = glue::glue("Cars with {cyl} cylinders"))
+  d_tab <- new_tab_tibble(d_tab_prep)
   expect_output(
     suppressWarnings(knit_print(d_tab)),
     regexp = "Cars with 8 cylinders"
